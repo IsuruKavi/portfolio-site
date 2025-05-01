c3bc2b96d8d1aaeb9ec9e116c66b5b611c2a8c1a
IsuruKavi <isurukavindalakshan863@gmail.com>
2025-05-01 23:03:14 +0530

    developing hero section

---
+++ components/ Hero.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import { cn } from "@/lib/utils";
+import { BackgroundBeamsWithCollision } from "./ui/background-beams-with-collision";
+import MagicButton from "./ui/MagicButton";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
+
+const Hero = () => {
+  return (
+    <BackgroundBeamsWithCollision>
+      <div className="pb-40 pt-46  w-full ">
+        <div className=" flex flex-col  h-full absolute  top-0 left-0 items-center justify-center bg-transparent w-full">
+          <div
+            className={cn(
+              "absolute inset-3 ",
+              "[background-size:80px_80px] ",
+              "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+              "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+            )}
+          />
+          {/* Radial gradient for the container to give a faded look */}
+          <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_1%,black)] dark:bg-black"></div>
+        
+            
+            <TextGenerateEffect
+              duration={2}
+              filter={false}
+              className="text-4xl text-center z-10 mb-4"
+              words={
+                "Turn Ideas Into Experiences — Not Just Things You See, But Things You Feel And Enjoy Using."
+              }
+            />
+            
+        
+          <p className="text-white md:text-2xl z-10">
+            Hi! I&apos;m Isuru Kavi , a Full stack mobile developer
+          </p>
+          <div className="my-8">
+            <MagicButton />
+          </div>
+        </div>
+      </div>
+    </BackgroundBeamsWithCollision>
+  );
+};
+
+export default Hero;
---
+++ components/ui/MagicButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FaLocationArrow } from "react-icons/fa6";
+function MagicButton() {
+  return (
+    <button className="relative inline-flex h-12 overflow-hidden rounded-2xl p-[1px] focus:outline-none items-center focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+      <span className="inline-flex h-full w-full gap-2 cursor-pointer items-center justify-center rounded-2xl bg-slate-950 px-4 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+        See my work
+        <FaLocationArrow /> 
+      </span>
+    </button>
+  );
+}
+
+export default MagicButton
